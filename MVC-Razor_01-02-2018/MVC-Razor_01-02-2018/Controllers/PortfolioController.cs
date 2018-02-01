56e4f473aac561557dbf99feef799dba9f3b357b
Orkhan Farajov <orkhankhf@code.edu.az>
2018-02-01 16:51:47 +0400

    Starting project

---
+++ MVC-Razor_01-02-2018/MVC-Razor_01-02-2018/Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Razor_01_02_2018.Models;
+
+namespace MVC_Razor_01_02_2018.Controllers
+{
+    public class PortfolioController : Controller
+    {
+		WebStudioEntities db = new WebStudioEntities();
+
+        // GET: Portfolio
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MVC-Razor_01-02-2018/MVC-Razor_01-02-2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Razor_01_02_2018.MvcApplication" Language="C#" %>
